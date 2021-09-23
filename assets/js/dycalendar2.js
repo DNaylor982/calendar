ec414efd9d9ca5db11677401c4e265ae909fb434
David <d.naylor982@gmail.com>
2021-09-23 16:09:04 +0200

    first commit

---
+++ assets/js/dycalendar2.js
@@ -0,0 +1,11 @@
+dycalendar.draw({
+    target: '#dycalendar',
+    type: 'month',
+    dayformat: 'full',
+    /*monthformat: 'full',*/
+    highlighttargetdate: true, /*vient cibler la date actuelle*/
+    prevnextbutton: 'show', /*flèche de défilement des mois */
+})
+
+
+
